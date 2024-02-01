a9cea84c55c3be94fec7a25623a3289e293c82e6
Basil <ohuruogubasil@gmail.com>
2024-02-01 22:08:58 +0100

    fix: dateOfDiscontinuation

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
-export const token = new URLSearchParams(window.location.search).get("jwt");
-export const url = "/api/v1/";
-// export const url = "http://localhost:8789/api/v1/"
-// export const token =
-//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA2NjMxNzM1fQ.JqHMmePSfAhrJ_X95vwvl5YqHn1to6s0JfL9FurF9OGCnWeAr_siYkhW7HOXCSa0PS1E8zqf7MYjrisFsCSS6g"
+// export const token = new URLSearchParams(window.location.search).get("jwt");
+// export const url = "/api/v1/";
+export const url = "http://localhost:8789/api/v1/";
+export const token =
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA2ODI5NTkzfQ.GXde68V-aMVoSVgTiKrbTE2Ej7oPz-lRvjh8Tfd-x4qYgaVRF8KDmVFNaK2QJ4_rHd_rJaqIEoWYrws2mTAOrA";
