eecbad5d0ce1375d913ffdeb6e53f4263010485b
Mathew Adegbite <mathewadegbite@gmail.com>
2024-02-02 11:09:29 +0100

    commenting out the API

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -2,4 +2,4 @@ export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
 // export const url = "http://localhost:8789/api/v1/"
 // export const token =
-//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA2NzQyOTk3fQ.5h6QdYv5idqrcGi2O0c2WLbs_etKDeGmPGo1czrB2yW8Gde1cQI1Ams_05vRgUlUeQ6yGLtmbsRPG-jGRkjhLw"
+//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA2ODk3MDM4fQ.tKS4ecJnOhuurtvj0nf_9SaQ0EkSjtkGBz7bUU4Kzju92qo-myx51vxjEe8Ey7zXpZuHTTB6xC_KRizH-jtzAA"
