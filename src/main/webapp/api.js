967aa8240cc1e957b3eaf5e328843bc8b9ff5146
Basil <ohuruogubasil@gmail.com>
2024-02-08 08:54:06 +0100

    fix:client-verification

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,5 @@
-export const token = new URLSearchParams(window.location.search).get("jwt");
-export const url = "/api/v1/";
+ export const token = new URLSearchParams(window.location.search).get("jwt");
+ export const url = "/api/v1/";
 // export const url = "http://localhost:8789/api/v1/"
 // export const token =
-//   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA2ODk3MDM4fQ.tKS4ecJnOhuurtvj0nf_9SaQ0EkSjtkGBz7bUU4Kzju92qo-myx51vxjEe8Ey7zXpZuHTTB6xC_KRizH-jtzAA"
+//  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA3MzQ3NTI4fQ.m2vtcnhSDf753ZXOHWCFXhr0nPxXhLLVty80UQBznyJJWrumu93mEmiVXcEqVnrfhf__XlJ1xbcGimAi2bmvHA";
--- src/main/webapp/jsx/components/History/PatientHistory.js
+++ src/main/webapp/jsx/components/History/PatientHistory.js
@@ -209,10 +209,10 @@ const PatientnHistory = (props) => {
         activeTab: "history",
         actionType: action,
       });
-      } else if (row.path === "client-verfication-form") {
+    } else if (row.path === "Client-Verification") {
       props.setActiveContent({
         ...props.activeContent,
-        route: "client-verfication-form-update",
+        route: "client-verfication-form",
         id: row.id,
         activeTab: "history",
         actionType: action,
@@ -226,7 +226,7 @@ const PatientnHistory = (props) => {
         actionType: action,
       });
     } else if (row.path === "eac-session") {
-      props.setActiveContent({ 
+      props.setActiveContent({
         ...props.activeContent,
         route: "eac-session-update",
         id: row.id,
@@ -266,6 +266,7 @@ const PatientnHistory = (props) => {
   };
   const LoadDeletePage = (row) => {
     console.log(row.path);
+
     if (row.path === "Mental-health") {
       setSaving(true);
       //props.setActiveContent({...props.activeContent, route:'mental-health-view', id:row.id})
@@ -600,7 +601,36 @@ const PatientnHistory = (props) => {
             toast.error("Something went wrong. Please try again...");
           }
         });
-    } else {
+    } else if (row.path === "Client-Verification") {
+      setSaving(true);
+      //props.setActiveContent({...props.activeContent, route:'mental-health-history', id:row.id})
+    
+      axios
+        .delete(`${baseUrl}hiv/art/clinic-visit/${row.id}/${reason.reason}`, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((response) => {
+          toast.success("Record Deleted Successfully");
+          PatientHistory();
+          toggle();
+          setSaving(false);
+        })
+        .catch((error) => {
+          setSaving(false);
+          if (error.response && error.response.data) {
+            let errorMessage =
+              error.response.data.apierror &&
+              error.response.data.apierror.message !== ""
+                ? error.response.data.apierror.message
+                : "Something went wrong, please try again";
+            toast.error(errorMessage);
+          } else {
+            toast.error("Something went wrong. Please try again...");
+          }
+        });
+    
+    }
+    else {
     }
   };
   const LoadModal = (row) => {
