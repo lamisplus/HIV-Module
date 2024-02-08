98f745ef63130dbba4cfa9555681237dc200030f
Basil <ohuruogubasil@gmail.com>
2024-02-08 11:25:47 +0100

    temporary-test-branch

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,5 +1,6 @@
  export const token = new URLSearchParams(window.location.search).get("jwt");
- export const url = "/api/v1/";
+export const url = "/api/v1/";
+ //TESTING
 // export const url = "http://localhost:8789/api/v1/"
 // export const token =
 //  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA3MzQ3NTI4fQ.m2vtcnhSDf753ZXOHWCFXhr0nPxXhLLVty80UQBznyJJWrumu93mEmiVXcEqVnrfhf__XlJ1xbcGimAi2bmvHA";
