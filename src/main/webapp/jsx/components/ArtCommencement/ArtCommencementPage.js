806b1dce519af06050197d117e5ca4c1aafb879b
Gamey001 <87187530+Gamey001@users.noreply.github.com>
2024-06-14 16:41:28 +0100

    fix art date validation

--- src/main/webapp/jsx/components/ArtCommencement/ArtCommencementPage.js
+++ src/main/webapp/jsx/components/ArtCommencement/ArtCommencementPage.js
@@ -574,7 +574,7 @@ const ArtCommencement = (props) => {
                         id="visitDate"
                         onChange={handleInputChange}
                         value={objValues.visitDate}
-                        // min="01-01-1980"
+                        min="01-01-1980"
                         max={moment(new Date()).format("YYYY-MM-DD")}
                         style={{
                           border: "1px solid #014D88",
@@ -605,6 +605,7 @@ const ArtCommencement = (props) => {
                         id="visitDate"
                         onChange={handleInputChange}
                         value={objValues.visitDate}
+                        // min={enrollDate}
                         max={moment(new Date()).format("YYYY-MM-DD")}
                         style={{
                           border: "1px solid #014D88",
