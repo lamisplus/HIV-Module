b54d4751be66d2f3651fc350628429a4985dfc97
Gamey001 <87187530+Gamey001@users.noreply.github.com>
2024-06-14 16:29:25 +0100

    added date validation fix for transferIn pt.

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,5 +4,5 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE4NjYzOTMwfQ.ZiSOCZptkOttaftaFIafi2_pZ0pVb25aMFqHGNbUi7CU50tIIy6y9zJJxpP0eQYb8zum81ojpboDnmDyj7l4fg"
+    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE4MzgzNjQ0fQ.VML5pZp8dTK8hSRAbYGGSzEERhkGfqWlajp88SHp6PoQK8rXp4a1sFhU3uJCr_dSJ5LCq1xe8dmJVz2sfXPVkw"
     : new URLSearchParams(window.location.search).get("jwt");
