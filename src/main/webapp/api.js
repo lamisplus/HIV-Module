00db41d74c3582490af9fa805f45adf4e7251161
Gamey001 <87187530+Gamey001@users.noreply.github.com>
2024-06-18 08:17:57 +0100

    added who to the carecard payload

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,5 +4,5 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE4NjYzOTMwfQ.ZiSOCZptkOttaftaFIafi2_pZ0pVb25aMFqHGNbUi7CU50tIIy6y9zJJxpP0eQYb8zum81ojpboDnmDyj7l4fg"
+    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzE4NzIwNDgxfQ.EI7gTn0WJhI8dyrd-Ry0MSFYy_M98bH67Y1b95AsY7CBeEM6bcyZcgKd2WvwI-97PHmzwRUQ8osG3Vp9GxAuoQ"
     : new URLSearchParams(window.location.search).get("jwt");
