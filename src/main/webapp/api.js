2113cf90f49ee0a4e7f6d799c2b381667f653e03
taiwo.gboyegun <taiwo.gboyegun@thepalladiumgroup.com>
2025-06-11 15:47:25 +0100

    api modified

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,5 +4,5 @@ export const url =
         : "/api/v1/";
 export const token =
     process.env.NODE_ENV === "development"
-        ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzQ5NTcxNDg1fQ.JB6w_z9Eer2rowMf8m4NssSFEWoPnDZHbMc-Ohas69r24cAAz-OMqMondm7WVy8ph3du1e_X9Rb0TRmQrvCJSw"
+        ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzQ5NjUzNjY0fQ.gF2BdtdE5bkIEeXhlKeBRZ7vWTCCLj2-5gqYqtLXRsdEq7cBzSYlsW0QKQaEyf6sRd-GxmsUBekvz6kziQxLRQ"
         : new URLSearchParams(window.location.search).get("jwt");
