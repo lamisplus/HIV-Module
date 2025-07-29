c02ac17d0d69d0cdcb18a2e2f439658d9a89940a
Emmanuel Chinonye Nnajiofor <emmanuel.nnajiofor@thepalladiumgroup.com>
2025-07-29 09:04:52 +0100

    WIP: minor

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,5 +4,5 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzUzODAzOTE1fQ.0wMZzRYv_FqF62_jccCGh1l06eX-m6hoW2TU2psMZSWMRZF3H4bfccMh_JfZMASsH_t1ktQt5YiyYHKB4G0UTA"
+    ? ""
     : new URLSearchParams(window.location.search).get("jwt");
