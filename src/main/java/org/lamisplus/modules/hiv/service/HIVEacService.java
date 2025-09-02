efcf991dcc23f71e61474d44e09dc32be36cf9ba
taiwogboyegun <taiwo.gboyegun@thepalladiumgroup.com>
2025-09-02 12:45:18 +0100

    JIRA-0000:[Refactor]tee

--- src/main/java/org/lamisplus/modules/hiv/service/HIVEacService.java
+++ src/main/java/org/lamisplus/modules/hiv/service/HIVEacService.java
@@ -43,6 +43,7 @@ public class HIVEacService {
 				.orElseThrow(() -> new EntityNotFoundException(Person.class, "id", String.valueOf(patientId)));
 
 		if (moduleService.exist("Lab")) {
+			System.out.println("tetsing");
 			List<LabEacInfo> patientAllEacs = hivEacRepository.getPatientAllEacs(patientId);
 			List<LocalDate> existingDates = hivEacRepository
 					.getAllByPersonAndArchived(person, 0)
