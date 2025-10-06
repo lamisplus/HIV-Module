d2e587bb6ed98b237e92cd3991fc84eac65335d9
gboyeguntaiwo <taiwo.gboyegun@thepalladiumgroup.com>
2025-10-06 14:24:39 +0100

    JIRA-0000:[] SYSTEM SETUP

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,14 +1,38 @@
+// xport const url =
+//     process.env.NODE_ENV === "development"
+//         ? "http://localhost:8789/api/v1/"
+//         : "/api/v1/";
+//
+// export const token =
+//     process.env.NODE_ENV === "development"
+//         ? process.env.REACT_APP_DEV_JWT
+//         : new URLSearchParams(window.location.search).get("jwt");
+//
+// console.log("Environment:", process.env.NODE_ENV);
+// console.log("Token:", token);
+// console.log("API URL:", url);
+// export const wsUrl =
+//     process.env.NODE_ENV === "development"
+//         ? "http://localhost:8789/websocket"
+//         : "/websocket";e
+
+
+
 export const url =
     process.env.NODE_ENV === "development"
         ? "http://localhost:8789/api/v1/"
         : "/api/v1/";
 
+// export const token =
+//     process.env.NODE_ENV === "development"
+//         ? process.env.REACT_APP_DEV_JWT
+//         : new URLSearchParams(window.location.search).get("jwt");
+
+export const wsUrl =
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:8789/websocket"
+        : "/websocket";
+
 export const token =
     process.env.NODE_ENV === "development"
-        ? process.env.REACT_APP_DEV_JWT
-        : new URLSearchParams(window.location.search).get("jwt");
-
-        export const wsUrl =
-        process.env.NODE_ENV === "development"
-          ? "http://localhost:8789/websocket"
-          : "/websocket";
+        ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluLFVzZXIsUkRFIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzU5NDk3NDY2fQ.LFVynL7MyAMsp5ptSZBjuv1srYSM2ZdLVvxLgSVy5IaAo7RRuQ_bKF-B9_onOZQNvnNEEkp5HkQ9-3RsBlOjUw"  : new URLSearchParams(window.location.search).get("jwt");
