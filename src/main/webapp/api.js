6fe3e3cfbf47683116467449bc9d7fcc8c6eee9d
Emmanuel Chinonye Nnajiofor <emmanuel.nnajiofor@thepalladiumgroup.com>
2025-12-02 08:00:30 +0100

    minor changes

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -11,13 +11,13 @@ export const wsUrl =
 
 export const audioTranscriptionUrl = process.env.NODE_ENV === "development"
 
-    ? "http://100.24.122.103:7860/api/v1"
+    ? "http://3.238.242.92:7860/api/v1"
 
-    : "http://100.24.122.103:7860/api/v1";
+    : "http://3.238.242.92:7860/api/v1";
 
 export const token =
     process.env.NODE_ENV === "development"
-        ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyZGUtdXNlciIsImF1dGgiOiJTdXBlciBBZG1pbixVc2VyLFJERSIsIm5hbWUiOiJyZGUtdXNlciBsYXN0bmFtZSIsImV4cCI6MTc2Mzk5NjA4N30.6HOjG3JhLoeR8A13lBmNbFnd3I3UHJasRF9DoGDIQMuQEZaDioburJsR2Bwfp7lAU3teh6mhYICKay7RGLfnFg":  
+        ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyZGUtdXNlciIsImF1dGgiOiJTdXBlciBBZG1pbixVc2VyLFJERSIsIm5hbWUiOiJyZGUtdXNlciBsYXN0bmFtZSIsImV4cCI6MTc2NDEyMTY4Nn0.BgcFr1JPVa99oaoUs9it70q6fffwzQttPXDsncMmf0zeFk03qrApouSY1hfVKpc5VG_PlZNM2l0e8bs1Tj6cPA":  
         new URLSearchParams(window.location.search).get("jwt");
 
 
