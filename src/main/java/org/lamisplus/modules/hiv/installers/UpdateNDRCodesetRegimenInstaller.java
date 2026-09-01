d446cedd624017766011bc02cea26fdcd3de4d91
gboyeguntaiwo <taiwo.gboyegun@thepalladiumgroup.com>
2026-09-01 14:14:13 +0100

    LV3-0000:[impl]: hiv_regimen ndr codeset mapping clean up continue

--- src/main/java/org/lamisplus/modules/hiv/installers/UpdateNDRCodesetRegimenInstaller.java
+++ src/main/java/org/lamisplus/modules/hiv/installers/UpdateNDRCodesetRegimenInstaller.java
@@ -7,7 +7,7 @@ import org.springframework.core.annotation.Order;
 @Order(24)
 @Installer(name = "update-ndr-codeset-regimen-installer",
         description = "Update ndr_code_set and hiv_regimen_resolver to replace 'ABC+3TC(or FTC)+DTG' with 'ABC+3TC+DTG'",
-        version = 5)
+        version = 7)
 public class UpdateNDRCodesetRegimenInstaller extends AcrossLiquibaseInstaller {
     public UpdateNDRCodesetRegimenInstaller() {
         super("classpath:installers/hiv/schema/update-ndr-codeset-regimen.xml");
